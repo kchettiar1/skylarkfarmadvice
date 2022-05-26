2a32be2eb969c8ab6996bf47816e37961ed74677
kchettiar1 <krayativ.kc@gmail.com>
2022-05-26 15:43:43 +0100

    js card section icon

--- app.js
+++ app.js
@@ -1,61 +1,3 @@
-// Menu-toggle
-// const menuBtn = document.querySelector('.menu-btn')
-// let menuOpen = false;
-
-// menuBtn.addEventListener('click', () => {
-//   if (!menuOpen) {
-//     menuBtn.classList.add('open');
-//     menuOpen = true
-//   } else {
-//     menuBtn.classList.remove('open');
-//     menuOpen = false;
-//   }
-// })
-
-// Nav Menu Animation
-// navItems = document.querySelector('.nav-items');
-// let navOpen = false;
-
-// menuBtn.addEventListener('click', () => {
-//   if (!navOpen) {
-//     navItems.classList.remove('collapse');
-//     navItems.classList.add('expand');
-//     navItems.classList.remove('zero-height');
-//     navOpen = true;
-//   } else {
-//     navItems.classList.remove('expand');
-//     navItems.classList.add('collapse');
-//     navOpen = false;
-//   }
-// })
-
-// Nav Menu Anchor Items Close Functionality
-// const navLink = document.querySelectorAll('.nav-link');
-
-// for (var i = 0; i < navLink.length; ++i) {
-//   navLink[i].addEventListener('click', () => {
-//     navItems.classList.remove('expand');
-//     navItems.classList.add('zero-height');
-//     navOpen = false;
-//     menuBtn.classList.remove('open');
-//     menuOpen = false;
-//   }
-//   )};
-
-
-  // Services Expand/Collapse
-// const stewardshipDescription = document.querySelectorAll('.stewardship-description');
-// console.log(stewardshipDescription);
-
-//   const stewardshipLink = document.querySelectorAll('.stewardship-link');
-//   console.log(stewardshipLink);
-
-// for (var i = 0; i < stewardshipLink.length; ++i) {
-//   stewardshipLink[i].addEventListener('click', () => {
-//     stewardshipDescription.classList.toggle('openDescription');
-//   }
-//   )};
-
 
 // FUNCTION Mobile Nav Menu Open/Close
 
@@ -81,6 +23,20 @@ for (let i = 0; i < menuLink.length; ++i) {
         contactBtn.classList.add('hidden');
       }
       )};
+
+
+  // Card Section Expand/Close
+const servicesDesc = document.querySelectorAll(".services-description");
+
+const expandIcon = document.querySelectorAll(".expand-icon");
+for (let i = 0; i < expandIcon.length; ++i) {
+ expandIcon[i].addEventListener("click", () => {
+  expandIcon[i].classList.toggle("fa-minus");
+  expandIcon[i].classList.toggle("fa-plus");
+  servicesDesc[i].classList.toggle("hidden");
+ });
+}
+
 
 
 // FOOTER
